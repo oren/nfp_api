fd64e761d0f95aaae5f193755b5077a1fbd751a0
Jonatan Nilsson <jonatan@nilsson.is>
2015-02-18 03:45:19 +0000

    huge update, basic google registration and profile

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var koaLogger = require('koa-bunyan-logger');
 var mask = require('koa-json-mask');
 var router = require('koa-router');
 var koaBody = require('koa-better-body')({fieldsKey: false});
+var jwt = require('koa-jwt');
 
 var config = require('./lib/config');
 var log = require('./lib/log');
@@ -26,13 +27,19 @@ app.use(koaLogger.requestLogger({
 
 app.use(cors({
   origin: true,
-  credentials: true
+  credentials: true,
+  maxAge: 60 * 60 * 24 * 14 //14 days
+}));
+app.use(jwt({
+  secret: config.get('jwt:secret'),
+  passthrough: true
 }));
 app.use(router(app));
 app.use(mask());
 
 //Routes
 var authentication = require('./lib/request-handlers/authentication');
+var authenticated = authentication.authenticated;
 app.post('/authenticate', koaBody, authentication.authenticate);
 
 var category = require('./lib/request-handlers/category');
@@ -40,6 +47,10 @@ app.get('/categories', category.categories);
 
 var nav = require('./lib/request-handlers/nav');
 app.get('/nav', nav.getNav);
+
+var profile = require('./lib/request-handlers/profile');
+app.get('/profile', authenticated, profile.getProfile);
+app.post('/profile', authenticated, koaBody, profile.updateProfile);
 
 var releases = require('./lib/request-handlers/release');
 app.get('/', releases.hello);
